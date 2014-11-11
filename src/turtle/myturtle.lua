c2b4633dc20a8df8268df3ed6462cd4a3c863a1a
Ale2610 <ale.proto00@gmail.com>
2014-11-11 13:39:16 +0100

    Update myturtle.lua

--- src/turtle/myturtle.lua
+++ src/turtle/myturtle.lua
@@ -1,1 +1,11 @@
 os.pullEvent = os.pullEventRaw
+
+if fs.exists("myturtle/id.conf") then
+  idConf = fs.open("myturtle/id.conf", "r")
+  local id = idConf.readLine()
+else
+  local id = nul
+end
+
+term.clear()
+term.setCursorPos(1,1)
